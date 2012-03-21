6ccb31e8ba54584fef8f219ad4261bd7f410a6ed
Dieter Maas <dieter.maas@gmx.net>
2012-03-21 11:37:26 +0100

    ende static pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Sample32app::Application.routes.draw do
   get "static_pages/home"
 
   get "static_pages/help"
-
+  get "static_pages/about"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
