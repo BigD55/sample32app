e7f75c1fc8d67c8f072ac5143881a56e9a419480
Dieter Maas <dieter.maas@gmx.net>
2012-03-21 13:00:35 +0100

    excercises 3.5

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -38,4 +38,16 @@ describe "Static pages" do
                     :text => "Ruby on Rails Tutorial Sample App | About Us")
     end
   end
+
+  describe "Contact page" do
+    it "should have the content 'Contact'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+     it "should have the title 'Contact'" do
+      visit '/static_pages/about'
+      page.should have_selector('title',
+                    :text => "Ruby on Rails Tutorial Sample App | Contact")
+    end
+  end
 end
