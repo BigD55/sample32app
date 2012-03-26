dd12a81dc611fa9458b9dc1b4b445a61c98118e8
Dieter Maas <dieter.maas@gmx.net>
2012-03-26 22:05:50 +0200

    users-index-fertig

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:edit, :index, :update]
+  before_filter :correct_user,   only: [:edit, :update]
   def show
     @user = User.find(params[:id])
+  end
+  def index
+    @users = User.all
   end
   def new
     @user = User.new
@@ -15,4 +20,27 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  def edit
+    
+  end
+  def update
+     
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  private
+
+    def signed_in_user
+      store_location
+      redirect_to signin_path, notice: "Please sign in." unless signed_in?
+    end
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -2,7 +2,23 @@ require 'spec_helper'
 
 describe "UserPages" do
   subject { page }
+  
+  describe "index" do
+    before do
+      sign_in FactoryGirl.create(:user)
+      FactoryGirl.create(:user, name: "Bob", email: "bob@example.com")
+      FactoryGirl.create(:user, name: "Ben", email: "ben@example.com")
+      visit users_path
+    end
 
+    it { should have_selector('title', text: 'All users') }
+
+    it "should list each user" do
+      User.all.each do |user|
+        page.should have_selector('li', text: user.name)
+      end
+    end
+  end
   describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
     before { visit user_path(user) }
@@ -51,15 +67,48 @@ describe "UserPages" do
 
         it { should have_selector('title', text: user.name) }
         it { should have_selector('div.alert.alert-success', text: 'Welcome') }
-<<<<<<< HEAD
-=======
+
         it { should have_link('Sign out') }
         describe "followed by signout" do
           before { click_link "Sign out" }
           it { should have_link('Sign in') }
         end
->>>>>>> signinout32
       end
+    end
+  end
+  describe "edit" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { sign_in user
+             visit edit_user_path(user) 
+           }
+
+    describe "page" do
+      it { should have_selector('h1',    text: "Update your profile") }
+      it { should have_selector('title', text: "Edit user") }
+      it { should have_link('change', href: 'http://gravatar.com/emails') }
+    end
+
+    describe "with invalid information" do
+      before { click_button "Save changes" }
+
+      it { should have_content('error') }
+    end
+    describe "with valid information" do
+      let(:new_name)  { "New Name" }
+      let(:new_email) { "new@example.com" }
+      before do
+        fill_in "Name",             with: new_name
+        fill_in "Email",            with: new_email
+        fill_in "Password",         with: user.password
+        fill_in "Confirm Password", with: user.password
+        click_button "Save changes"
+      end
+
+      it { should have_selector('title', text: new_name) }
+      it { should have_selector('div.alert.alert-success') }
+      it { should have_link('Sign out', :href => signout_path) }
+      specify { user.reload.name.should  == new_name }
+      specify { user.reload.email.should == new_email }
     end
   end
 end
