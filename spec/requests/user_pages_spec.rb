dd6605b19702b371f99cfbca76cdd3741ca3fe9f
Dieter Maas <dieter.maas@gmx.net>
2012-03-26 11:23:05 +0200

    finished signinout

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -39,6 +39,18 @@ describe "UserPages" do
           click_button "Create my account"
         end.to change(User, :count).by(1)
       end
+      describe "after saving the user" do
+        before { click_button "Create my account" }
+        let(:user) { User.find_by_email('user@example.com') }
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_link('Sign out') }
+        describe "followed by signout" do
+          before { click_link "Sign out" }
+          it { should have_link('Sign in') }
+        end
+      end
     end
   end
 end
